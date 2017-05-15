9f4516d575ffc62a243ef0469f5091c6ce6e3a48
Katie Starrett <starrett.k.e@gmail.com>
2017-05-15 18:31:46 -0400

    add button addition functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
 var wins = 0;
 var losses = 0;
 var compGuess;
-var crystalValue1;
-var crystalValue2;
-var crystalValue3;
-var crystalValue4;
+var crystalValue1 = "";
+var crystalValue2 = "";
+var crystalValue3 = "";
+var crystalValue4 = "";
+var totalSoFar = 0;
 
 var getCompGuess = function () {
 	compGuess = Math.floor(Math.random() * 102) + 19;
@@ -19,7 +20,7 @@ var getCrystalValues = function () {
 	crystalValue3 = Math.floor(Math.random() * 12) +1;
 	crystalValue4 = Math.floor(Math.random() * 12) +1;
 
-	if (crystalValue1 === crystalValue2) {
+/*	if (crystalValue1 === crystalValue2) {
 		crystalValue2 = Math.floor(Math.random() * 12) +1;
 	}
 	else if (crystalValue3 === crystalValue2 || crystalValue3 === crystalValue1) {
@@ -27,13 +28,44 @@ var getCrystalValues = function () {
 	}
 	else if (crystalValue4 === crystalValue3 || crystalValue4 === crystalValue2 || crystalValue4 === crystalValue1) {
 		crystalValue4 = Math.floor(Math.random() * 12) +1;
-	}
+	}*/
 	console.log(crystalValue1, crystalValue2, crystalValue3, crystalValue4);
 };
 
-
 getCrystalValues();
 getCompGuess();
+
+if (totalSoFar < compGuess) {
+	$("#btn-1").on("click", function() {
+		totalSoFar = totalSoFar + crystalValue1;
+		$("#current-score").html(totalSoFar);
+	});
+
+	$("#btn-2").on("click", function() {
+		totalSoFar = totalSoFar + crystalValue2;
+		$("#current-score").html(totalSoFar);
+	});
+
+	$("#btn-3").on("click", function() {
+		totalSoFar = totalSoFar + crystalValue3;
+		$("#current-score").html(totalSoFar);
+	});
+
+	$("#btn-4").on("click", function() {
+		totalSoFar = totalSoFar + crystalValue4;
+		$("#current-score").html(totalSoFar);
+	});
+};
+else if (totalSoFar === compGuess) {
+	wins++;
+	$("#wins").html(wins);
+};
+else if (totalSoFar > compGuess) {
+	losses++;
+	$("#losses").html(losses);
+};
+
+
 
 
 
